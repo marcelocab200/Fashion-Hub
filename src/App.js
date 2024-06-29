919648c5e3fbc3eff9f9c304ed91ff922cce85fb
marcelocab200 <marcelocab200@gmail.com>
2024-06-29 20:04:09 -0300

    Finishing header and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
+import facebookIcon from './images/Icon facebook square.png';
+import twitterIcon from './images/Icon twitter.png';
+import instagramIcon from './images/Icon instagram.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <a href="">Fashion Hub</a>
+        <nav>
+          <ul>
+            <a href="">Início</a>
+            <a href="">Loja</a>
+            <a href="">Contatos</a>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <div className="Filter-options">
+        </div>
+      </main>
+      <footer>
+          <div className="Rights-section">
+            <p>Fashion Hub</p>
+            <p id="Copyright">@ 2023 Fashion Hub. Todos direitos reservados</p>
+          </div>
+          <div className="Social-media">
+            <p>Siga a gente</p>
+            <div>
+              <a href=""><img src={facebookIcon} alt="Facebook"/></a>
+              <a href=""><img src={twitterIcon} alt="Twitter"/></a>
+              <a href=""><img src={instagramIcon} alt="Instagram"/></a>
+            </div>
+          </div>
+      </footer>
     </div>
   );
 }
