64c070247a3d98e7abc4579a997eccb4f0a0fe22
marcelocab200 <marcelocab200@gmail.com>
2024-06-29 23:10:45 -0300

    Filters options section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
+import './App.css';
+
 import facebookIcon from './images/Icon facebook square.png';
 import twitterIcon from './images/Icon twitter.png';
 import instagramIcon from './images/Icon instagram.png';
-import './App.css';
 
 function App() {
   return (
@@ -19,6 +19,50 @@ function App() {
       </header>
       <main>
         <div className="Filter-options">
+          <p>Filtrar</p>
+          <div className="Categories">
+            <p>Categorias</p>
+            <input type="checkbox" id="category1" name="category1" value="Masculino"/>
+            <label>Masculino</label><br/>
+            <input type="checkbox" id="category2" name="category2" value="Feminino"/>
+            <label>Feminino</label><br/>
+            <input type="checkbox" id="category3" name="category3" value="Infantil"/>
+            <label>Infantil</label><br/>
+          </div>
+
+          <div className="Sizes">
+            <p>Tamanhos</p>
+            <input type="checkbox" id="size1" name="size1" value="PP"/>
+            <label>PP</label><br/>
+            <input type="checkbox" id="size2" name="size2" value="P"/>
+            <label>P</label><br/>
+            <input type="checkbox" id="size3" name="size3" value="M"/>
+            <label>M</label><br/>
+            <input type="checkbox" id="size4" name="size4" value="G"/>
+            <label>G</label><br/>
+            <input type="checkbox" id="size5" name="size5" value="GG"/>
+            <label>GG</label><br/>
+          </div>
+
+          <div className="Colors">
+            <p>Cores</p>
+            <input type="checkbox" id="color1" name="color1" value="Vermelho"/>
+            <label>Vermelho</label><br/>
+            <input type="checkbox" id="color2" name="color2" value="Azul"/>
+            <label>Azul</label><br/>
+            <input type="checkbox" id="color3" name="color3" value="Verde"/>
+            <label>Verde</label><br/>
+          </div>
+
+          <div className='Price-range'>
+            <p>Faixa de preço</p>
+            <div>
+              <p>R$0</p>
+              <p>R$1000</p>
+            </div>
+            <input type="range" min="0" max="1000" value="500"/>
+          </div>
+          <button type="button">Aplicar</button>
         </div>
       </main>
       <footer>
